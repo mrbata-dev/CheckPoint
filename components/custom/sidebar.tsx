1d805327c0592624aedeb5423117215dc2559da2
secret <lordhunter017@gmail.com>
2025-09-04 12:23:30 +0545

    completed?

--- components/custom/sidebar.tsx
+++ components/custom/sidebar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { LogOut, Menu, X } from 'lucide-react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+
 import React, { useState } from 'react'
 import { Button } from '../ui/button'
 import { signOut } from 'next-auth/react'
